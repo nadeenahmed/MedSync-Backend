2d6bf42f85616bdc96b25fbcbc4cb8fb0b6693e3
Nadeen Ahmed <74678959+nadeenahmed@users.noreply.github.com>
2024-01-27 20:07:40 +0200

    recaptcha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -12,7 +12,9 @@ use App\Models\EmergencyData;
 use Illuminate\Support\Facades\Hash;
 use App\Notifications\RegisterationNotification;
 use App\Notifications\EmailVerificationNotification;
+use Closure;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Http;
 
 
 
@@ -32,8 +34,74 @@ class RegisterController extends Controller
             $newuser['status'] = 'pending';
             $user = User::create($newuser);
             $token = $user->createToken('user',['app:all'])->plainTextToken;
+            // $g_recaptcha_response = function(string $attribute, mixed $value, Closure $fail){
+            //     $g_response = Http::asForm->post("https://www.google.com/recaptcha/api/siteverify",[
+            //         'secret' => config('services.recaptcha.secret_key'),
+            //         'response' => $value,
+            //     ]);
+            //     if(!$g_response->json('success')){
+            //         $fail("The {$attribute} is invalid");
+            //     }
+            // };
+            // $url = 'https://www.google.com/recaptcha/api/siteverify';
+            // $remoteip = $_SERVER['REMOTE_ADDR'];
+            // $data = [
+            //         'secret' => config('services.recaptcha.secret_key'),
+            //         'response' => $request->get('recaptcha'),
+            //         'remoteip' => $remoteip
+            //     ];
+            // $options = [
+            //         'http' => [
+            //         'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            //         'method' => 'POST',
+            //         'content' => http_build_query($data)
+            //         ]
+            //     ];
+            // $context = stream_context_create($options);
+            //         $result = file_get_contents($url, false, $context);
+            //         $resultJson = json_decode($result);
+            // if ($resultJson->success != true) {
+            //         return back()->withErrors(['captcha' => 'ReCaptcha Error']);
+            //         }
+            // if ($resultJson->score >= 0.3) {
+            //         //Validation was successful, add your form submission logic here
+            //         return back()->with('message', 'Thanks for your message!');
+            // } else {
+            //         return back()->withErrors(['captcha' => 'ReCaptcha Error']);
+            // }
+//             $secret = 'paste-your-secret-key-here';
 
-        
+
+
+// $ch = curl_init();
+
+
+
+//     curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+
+//     curl_setopt($ch, CURLOPT_POST, 1);
+
+//     curl_setopt($ch, CURLOPT_POSTFIELDS,
+
+// "secret=".$secret."&response=".$data['g-recaptcha-response']);
+
+    
+
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//     $result = curl_exec($ch);
+
+//     $responseData = json_decode($result , TRUE);
+
+//     curl_close ($ch);
+
+
+
+//     if($responseData['success'] == false){
+
+//         return back();
+
+//     }
             $user->notify(new EmailVerificationNotification());
             if ($newuser['role'] === 'patient') {
                 $patient = Patient::create([
