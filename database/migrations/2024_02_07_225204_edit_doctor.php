138d6d454093b691ee0cb836630f4ae733c76c12
Nadeen Ahmed <74678959+nadeenahmed@users.noreply.github.com>
2024-02-13 03:37:46 +0200

    change

--- database/migrations/2024_02_07_225204_edit_doctor.php
+++ database/migrations/2024_02_07_225204_edit_doctor.php
@@ -16,12 +16,7 @@ return new class extends Migration
                 $table->json('clinic_addresses')->nullable()->change();
                 $table->json('clinic_phones')->nullable()->change();
                 $table->string('license_path')->nullable();
-                
-                degree: The medical degree obtained by the professional.
-university: The university from which the professional graduated.
-board_certified: Boolean indicating whether the professional is board-certified.
-certifying_board: 
-        });
+    });
     }
 
     /**
